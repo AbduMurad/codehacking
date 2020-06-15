5c23c1617ddbba1c86b8449ade3b8526f50c0887
Abdu <abadeem@gmail.com>
2020-06-15 10:01:04 +0200

    create user page now can upload a file

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,6 +3,46 @@
 
 @section('content')
 
-    <h1>Create User:</h1>
+    <div class="row">
+        <div class="col-sm-6">
+            <h1>Create A User</h1>
+
+            {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+            <div class="form-group">
+                {!! Form::label('name', 'Name:') !!}
+                {!! Form::text('name', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('email', 'Email:') !!}
+                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('status', 'Status:') !!}
+                {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), '0', ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('role_id', 'Role:') !!}
+                {!! Form::select('role_id', array(''=>'Choose Role') + $roles, '0', ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('file', 'Avatar:') !!}
+                {!! Form::file('file', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('password', 'Password:') !!}
+                {!! Form::password('password', ['class'=>'form-control']) !!}
+                {!! Form::label('password_confirm', 'Password Confirmation:') !!}
+                {!! Form::password('password_confirmation', ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+            </div>
+            {!! Form::close() !!}
+        </div>
+    </div>
+    <div class="row">
+        @include('includes.form-error')
+    </div>
 
 @endsection
